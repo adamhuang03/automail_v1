85afec52da5c09c6143f75807f748ce6ab3070f7
Adam Huang <johndoe@example.com>
2024-10-14 06:14:43 -0400

    1.0.4: Final Testing

--- src/app/api/auth/login/callback/page.tsx
+++ src/app/api/auth/login/callback/page.tsx
@@ -29,10 +29,18 @@ export default function AuthCallback() {
         document.cookie = `sb-access-token=${accessToken}; path=/; secure; samesite=lax`;
         document.cookie = `sb-refresh-token=${refreshToken}; path=/; secure; samesite=lax`;
 
-        router.replace('/onboard');
+        const { data } = await supabase.auth.getSession();
+        const session = data.session;
+
+        if (session) {
+          const { error } = await supabase.from('user_profile')
+          .update({ provider_token: session.provider_token, provider_refresh_token: session.provider_refresh_token})
+          .eq('id', session.user.id);
+        }
+
+        router.replace('/home');
       } else {
-        console.log('na')
-        // router.replace('/login');
+        router.replace('/login');
       }
     })();
   }, [router]);
