35ad7f2586d9a15302d04b86d0c38305cfd099ec
Adam Huang <johndoe@example.com>
2024-10-25 00:51:49 -0400

    Refactor gmail sending code on api side

--- src/app/api/send-scheduled-emails/route.ts
+++ src/app/api/send-scheduled-emails/route.ts
@@ -6,6 +6,11 @@ import { OutreachUser } from '@/utils/types';
 import { sendEmailWithPdfFromUrl, sendEmail, refreshAccessToken } from '@/utils/google/emailGoogle';
 
 export async function POST() {
+  const response = await processScheduledEmails();
+  return NextResponse.json(response, { status: response.status });
+}
+
+async function processScheduledEmails() {
   const currentTime = new Date().toISOString().slice(0, 16);
 
   // Fetch scheduled emails
@@ -20,7 +25,7 @@ export async function POST() {
 
   if (error) {
     console.error('Error fetching emails:', error);
-    return NextResponse.json({ message: 'Error fetching scheduled emails', error }, { status: 500 });
+    return { message: 'Error fetching scheduled emails', error, status: 500 };
   }
 
   if (emails && emails.length > 0) {
@@ -33,7 +38,6 @@ export async function POST() {
       oAuth2Client.setCredentials({ access_token: accessToken });
 
       try {
-
         if (resumeLink) {
           await sendEmailWithPdfFromUrl(
             oAuth2Client, 
@@ -45,28 +49,20 @@ export async function POST() {
           await supabase.from('outreach').update({ status: 'Sent w Attachment' }).eq('id', email.id);
           console.log(`Email w attachment sent to ${email.to_email}`);
         } else {
-          
-          // Test if the access token is valid by sending the email
           await sendEmail(oAuth2Client, email.to_email, email.subject_generated, email.email_generated);
-          // Mark email as sent in the database
           await supabase.from('outreach').update({ status: 'Sent' }).eq('id', email.id);
           console.log(`Email sent to ${email.to_email}`);
-
         }
-
       } catch (error) {
         console.log('Access token expired. Refreshing...');
         try {
-          // Refresh the access token using the refresh token
           const newAccessToken = await refreshAccessToken(refreshToken);
 
-          // Save the new token in the database
           await supabase
             .from('user_profile')
             .update({ provider_token: newAccessToken })
             .eq('id', email.user_profile_id);
 
-          // Retry sending the email with the new access token
           oAuth2Client.setCredentials({ access_token: newAccessToken });
           await sendEmail(oAuth2Client, email.to_email, email.subject_generated, email.email_generated);
           await supabase.from('outreach').update({ status: 'Sent' }).eq('id', email.id);
@@ -78,5 +74,5 @@ export async function POST() {
     }
   }
 
-  return NextResponse.json({ message: 'Scheduled emails processed' }, { status: 200 });
+  return { message: 'Scheduled emails processed', status: 200 };
 }
