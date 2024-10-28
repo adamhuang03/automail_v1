41b14baad5c303a68791bf08ae3843e46751320b
Adam Huang <johndoe@example.com>
2024-10-28 12:30:13 -0400

    Final update push

--- src/app/test/signup/page.tsx
+++ src/app/test/signup/page.tsx
@@ -2,40 +2,9 @@
 import { KBarProvider, KBarPortal, KBarPositioner, KBarAnimator, KBarSearch } from 'kbar'
 import { useEffect } from 'react'
 
-function Page() {
-  const actions = [
-    {
-      id: 'blog',
-      name: 'Blog',
-      shortcut: ['b'],
-      keywords: 'writing words',
-      perform: () => (window.location.pathname = 'blog'),
-    },
-    // Add more actions here
-  ]
-
-  // useEffect(() => {
-  //   const down = (e: KeyboardEvent) => {
-  //     if (e.key === '/' && (e.metaKey || e.ctrlKey)) {
-  //       e.preventDefault()
-  //       kbar.query.toggle()
-  //     }
-  //   }
-  
-  //   document.addEventListener('keydown', down)
-  //   return () => document.removeEventListener('keydown', down)
-  // }, [])
+export default function Page() {
 
   return (
-    <KBarProvider actions={actions}>
-      <KBarPortal>
-        <KBarPositioner>
-          <KBarAnimator>
-            <KBarSearch />
-            {/* Add more kbar components here */}
-          </KBarAnimator>
-        </KBarPositioner>
-      </KBarPortal>
-    </KBarProvider>
+    <div className="">hi</div>
   )
 }
