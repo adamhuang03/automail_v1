471bde83deca7ee3f49586eca4985e0ba42bcf2e
Adam Huang <johndoe@example.com>
2024-10-28 12:26:05 -0400

    Final update push

--- src/app/test/signup/page.tsx
+++ src/app/test/signup/page.tsx
@@ -1,41 +1,41 @@
-// 'use client'
-// import { KBarProvider, KBarPortal, KBarPositioner, KBarAnimator, KBarSearch } from 'kbar'
-// import { useEffect } from 'react'
+'use client'
+import { KBarProvider, KBarPortal, KBarPositioner, KBarAnimator, KBarSearch } from 'kbar'
+import { useEffect } from 'react'
 
-// function Page() {
-//   const actions = [
-//     {
-//       id: 'blog',
-//       name: 'Blog',
-//       shortcut: ['b'],
-//       keywords: 'writing words',
-//       perform: () => (window.location.pathname = 'blog'),
-//     },
-//     // Add more actions here
-//   ]
+function Page() {
+  const actions = [
+    {
+      id: 'blog',
+      name: 'Blog',
+      shortcut: ['b'],
+      keywords: 'writing words',
+      perform: () => (window.location.pathname = 'blog'),
+    },
+    // Add more actions here
+  ]
 
-//   // useEffect(() => {
-//   //   const down = (e: KeyboardEvent) => {
-//   //     if (e.key === '/' && (e.metaKey || e.ctrlKey)) {
-//   //       e.preventDefault()
-//   //       kbar.query.toggle()
-//   //     }
-//   //   }
+  // useEffect(() => {
+  //   const down = (e: KeyboardEvent) => {
+  //     if (e.key === '/' && (e.metaKey || e.ctrlKey)) {
+  //       e.preventDefault()
+  //       kbar.query.toggle()
+  //     }
+  //   }
   
-//   //   document.addEventListener('keydown', down)
-//   //   return () => document.removeEventListener('keydown', down)
-//   // }, [])
+  //   document.addEventListener('keydown', down)
+  //   return () => document.removeEventListener('keydown', down)
+  // }, [])
 
-//   return (
-//     <KBarProvider actions={actions}>
-//       <KBarPortal>
-//         <KBarPositioner>
-//           <KBarAnimator>
-//             <KBarSearch />
-//             {/* Add more kbar components here */}
-//           </KBarAnimator>
-//         </KBarPositioner>
-//       </KBarPortal>
-//     </KBarProvider>
-//   )
-// }
+  return (
+    <KBarProvider actions={actions}>
+      <KBarPortal>
+        <KBarPositioner>
+          <KBarAnimator>
+            <KBarSearch />
+            {/* Add more kbar components here */}
+          </KBarAnimator>
+        </KBarPositioner>
+      </KBarPortal>
+    </KBarProvider>
+  )
+}
